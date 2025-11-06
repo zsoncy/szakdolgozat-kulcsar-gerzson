cb225442f17cb943b46b6ce8866aa7a011dbc7b8
Kulcsár Gerzson <zsoncy@mailbox.unideb.hu>
2025-11-07 01:40:03 +0200

    Implementing the select equation page and integrating it into the select calculation page.

--- frontend/select_calc.py
+++ frontend/select_calc.py
@@ -2,6 +2,8 @@ from customtkinter import *
 
 from frontend.page import Page
 
+from frontend.select_eq import Select_Eq_Page
+
 
 class Select_Calc_Page(Page):
     def __init__(self, root, *args, **kwargs):
@@ -11,8 +13,14 @@ class Select_Calc_Page(Page):
         root.columnconfigure(0, weight=1)
         root.columnconfigure(1, weight=1)
 
+        def page1():
+            button_1.destroy()
+            button_2.destroy()
+            button_3.destroy()
+            Select_Eq_Page(self.root)
+
         button_1 = CTkButton(master=root, text="Equation", fg_color="#4e1d58", hover_color="#370d40",
-                             text_color="#DDC3C3", font=("Helvetica", 40), corner_radius=50)
+                             text_color="#DDC3C3", font=("Helvetica", 40), corner_radius=50, command = page1)
         button_1.grid(row=0, column=0, sticky="ew", padx=100, pady=400, ipady=20, ipadx=20)
 
         button_2 = CTkButton(master=root, text="Calculator", fg_color="#4e1d58", hover_color="#370d40",
